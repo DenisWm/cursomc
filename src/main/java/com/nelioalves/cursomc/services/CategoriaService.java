26b1b0862be771a46a474293f7d332ef2e44e0aa
DenisWm <denismamoni@hotmail.com>
2022-06-03 00:04:18 -0400

    Produto e associação muitos para muitos

--- src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -6,13 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nelioalves.cursomc.domain.Categoria;
-import com.nelioalves.cursomc.repositories.CategoriaRepositoty;
+import com.nelioalves.cursomc.repositories.CategoriaRepository;
 
 @Service
 public class CategoriaService {
 	
 	@Autowired
-	private CategoriaRepositoty repo;
+	private CategoriaRepository repo;
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
